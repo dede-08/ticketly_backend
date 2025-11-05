8006c6f0a1ddc377a1782755aacb95303a34eab6
dede-08 <benandrealanyaguevara@gmail.com>
2025-11-04 19:26:41 -0500

    updats

--- manage.py
+++ manage.py
@@ -8,6 +8,11 @@ def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ticketly_backend.settings')
     try:
+        from dotenv import load_dotenv
+        load_dotenv()
+    except ImportError:
+        pass
+    try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
